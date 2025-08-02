92c9a318e7d5a4629b24fab170ca6ac4ccb9bddb
Akan-86 <akan.degirmenci@gmail.com>
2025-08-02 09:52:13 +0300

    add Firestore API key from Firebase console

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,10 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "...",
-  authDomain: "...",
-  projectId: "...",
-  
+  apiKey: "AIzaSyATHSx-b1Z6M7xW8rdsTuDN7yjTeY-IBMI",
+  authDomain: "random-quety-react-app.firebaseapp.com",
+  projectId: "random-quety-react-app",
+  storageBucket: "random-quety-react-app.firebasestorage.app",
+  messagingSenderId: "519096057012",
+  appId: "1:519096057012:web:00ecd95006d3c260e62be1",
+  measurementId: "G-75206S1Y9D"
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
